746d71330c09e0246efb87f832ae4d1bdda22c1d
JoaoB <j.bosco1994@live.com>
2026-04-23 15:22:22 -0300

    adicionado uma linha teste

--- src/ModuloQuatro.java
+++ src/ModuloQuatro.java
@@ -1,5 +1,6 @@
 public class ModuloQuatro {
     public static void main(String[] args) {
         System.out.println("Teste do Módulo Quatro");
+        System.out.println("Se aparecer, funcionou");
     }
 }
